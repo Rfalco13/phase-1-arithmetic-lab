6cb5014ac3657a271b420de63d0af2545377e089
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-05-18 19:10:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num2 * 2; 
+const random = 1;
+const num3 = 2 * 2;
+const num4 = 2;
+const mod = num4 * 2;
+const max = 20;
